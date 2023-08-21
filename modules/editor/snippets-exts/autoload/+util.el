e1806ebeaf60644d0e938e44c5be2d390ca57473
Chris Barrett <chris@walrus.cool>
2023-08-21 22:36:35 +1200

    Set `mode` file-local-variable for all snippets

---
+++ modules/editor/snippets-exts/autoload/+util.el
@@ -0,0 +1,14 @@
+;;; editor/snippets-exts/autoload/+util.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun +snippets-each-file (fn)
+  "Iterate over snippets calling FN on each.
+
+FN is called with a single argument, which is the absolute path
+of the current snippet. It is called in a temp buffer with that
+snippet's contents."
+  (dolist (file (directory-files-recursively +snippets-dir (rx (* any))))
+    (with-temp-buffer
+      (let ((require-final-newline nil))
+        (insert-file-contents file)
+        (funcall fn file)))))
