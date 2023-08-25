940a9de30d2a11a9616a24962bb165d3c4855e65
Chris Barrett <chris@walrus.cool>
2023-08-25 14:08:31 +1200

    Teach format-all about nixpkgs-fmt

--- modules/tools/nix/config.el
+++ modules/tools/nix/config.el
@@ -2,3 +2,10 @@
 
 (defvar +nix-config-directory "~/.config/nixpkgs"
   "Directory containing the user's nix configuration.")
+
+(after! format-all
+  (define-format-all-formatter nixpkgs-fmt
+                               (:executable "nixpkgs-fmt")
+                               (:install "nix-env -f https://github.com/nix-community/nixpkgs-fmt/archive/master.tar.gz -i")
+                               (:modes nix-mode)
+                               (:format (format-all--buffer-easy executable))))
