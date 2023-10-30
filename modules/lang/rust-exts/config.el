5fcec7fb762a3ac074a00a76b927f4123595bfeb
Chris Barrett <chris@walrus.cool>
2023-10-30 22:05:11 +1300

    changes from walrus.local on Mon Oct 30 22:05:11 NZDT 2023

--- modules/lang/rust-exts/config.el
+++ modules/lang/rust-exts/config.el
@@ -1,11 +1,4 @@
 ;; -*- lexical-binding: t; -*-
 
 (after! (:and rustic-babel ob)
-  ;; Add a `:profile' header arg for specifying the build profile.
-  (define-advice rustic-make-process (:filter-args (args) apply-profile)
-    (setf (plist-get args :command)
-          (append
-           (plist-get args :command)
-           (when-let* ((profile (alist-get :profile rustic-babel-params)))
-             (list (format "--%s" profile)))))
-    args))
+  (setq rustic-babel-default-toolchain "nightly"))
