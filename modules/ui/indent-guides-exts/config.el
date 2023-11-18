68c7354a5a5d1b1ac0b3adcd12d96d0f2e71db32
Chris Barrett <chris@walrus.cool>
2023-11-18 21:23:57 +1300

    Enable highlight-indent-guides in yaml-ts-mode

--- modules/ui/indent-guides-exts/config.el
+++ modules/ui/indent-guides-exts/config.el
@@ -1,7 +1,7 @@
 ;;; ui/indent-guides-exts/config.el -*- lexical-binding: t; -*-
 
 (remove-hook! (prog-mode text-mode conf-mode) 'highlight-indent-guides-mode)
-(add-hook! (yaml-mode nxml-mode python-ts-mode) 'highlight-indent-guides-mode)
+(add-hook! (yaml-mode yaml-ts-mode nxml-mode python-ts-mode) 'highlight-indent-guides-mode)
 
 (after! highlight-indent-guides
   (setq highlight-indent-guides-method 'fill)
