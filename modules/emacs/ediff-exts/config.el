2188fb9300b2f5d2e41e360fd0db4992560ae600
Chris Barrett <chris@walrus.cool>
2023-11-18 21:24:19 +1300

    Enable ediff-show-clashes-only

--- modules/emacs/ediff-exts/config.el
+++ modules/emacs/ediff-exts/config.el
@@ -4,6 +4,8 @@
   (advice-add 'ediff-next-difference :after #'+ediff-reveal-org-content-around-hunk)
   (advice-add 'ediff-previous-difference :after #'+ediff-reveal-org-content-around-hunk)
 
+  (setq ediff-show-clashes-only t)
+
   (add-hook! 'ediff-keymap-setup-hook
              ;; Keymap is buffer-local
              (map! :map ediff-mode-map "B" #'+ediff-copy-both-to-C)))
