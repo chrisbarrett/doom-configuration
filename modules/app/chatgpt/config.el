284fc98b85823eee56dadbc2e8695be3e4902b4b
Chris Barrett <chris@walrus.cool>
2023-11-30 23:10:46 +1300

    Use newer gpt4 model

--- modules/app/chatgpt/config.el
+++ modules/app/chatgpt/config.el
@@ -4,7 +4,11 @@
   (setq chatgpt-display-function #'display-buffer)
   (setq chatgpt-shell-openai-key
         (lambda ()
-          (auth-source-pick-first-password :host "api.openai.com"))))
+          (auth-source-pick-first-password :host "api.openai.com")))
+
+  (let ((gpt-latest "gpt-4-1106-preview" ))
+    (setq chatgpt-shell-model-version gpt-latest)
+    (add-to-list 'chatgpt-shell-model-versions gpt-latest 'append)))
 
 (after! ob-chatgpt-shell
   (ob-chatgpt-shell-setup))
