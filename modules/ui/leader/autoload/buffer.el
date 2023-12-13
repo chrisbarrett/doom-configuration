326f332e03c89d8480e91fa32df5fd6eef99f138
Chris Barrett <chris@walrus.cool>
2023-12-13 21:03:35 +1300

    Constrain +swap-buffers to switching non-popups

--- modules/ui/leader/autoload/buffer.el
+++ modules/ui/leader/autoload/buffer.el
@@ -3,15 +3,17 @@
 ;;;###autoload
 (defun +swap-buffers (&optional window)
   (interactive)
-  ;; TODO: Check whether this is really very different from the command provided by evil.
-  (let ((current-buffer (window-buffer window))
-        (buffer-predicate (frame-parameter (window-frame window) 'buffer-predicate)))
+  (let* ((buffer-a (window-buffer window))
+         (buffer-predicate (frame-parameter (window-frame window) 'buffer-predicate))
+         (popup-p (+popup-buffer-p buffer-a)))
     ;; switch to first buffer previously shown in this window that matches
     ;; frame-parameter `buffer-predicate'
     (switch-to-buffer
-     (or (car (seq-filter (lambda (buffer)
-                            (and (not (eq buffer current-buffer))
-                                 (or (null buffer-predicate) (funcall buffer-predicate buffer))))
+     (or (car (seq-filter (lambda (buffer-b)
+                            (and (not (eq buffer-b buffer-a))
+                                 (equal popup-p (+popup-buffer-p buffer-b))
+                                 (or (null buffer-predicate)
+                                     (funcall buffer-predicate buffer-b))))
                           (seq-map #'car (window-prev-buffers window))))
          ;; `other-buffer' honors `buffer-predicate' so no need to filter
-         (other-buffer current-buffer t)))))
+         (other-buffer buffer-a t)))))
