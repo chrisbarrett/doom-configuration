3e0f7a7d3bc0327790b105acab9e5c369ad26beb
Chris Barrett <chris@walrus.cool>
2023-12-26 13:40:16 +1300

    Fix up agenda views to show last few days

--- modules/lang/org-exts/+agenda.el
+++ modules/lang/org-exts/+agenda.el
@@ -1,5 +1,11 @@
 ;;; lang/org-exts/+agenda.el -*- lexical-binding: t; -*-
 
+;;; Global settings
+
+(setq org-agenda-files (expand-file-name "org-agenda-files" org-directory))
+(setq org-agenda-text-search-extra-files `(agenda-archives ,(expand-file-name "archive.org" org-directory)))
+(setq org-agenda-search-view-always-boolean t)
+
 ;;; Configure my agenda views.
 
 ;; The todos shown in the agenda are aggressively filtered using skip functions, so
@@ -20,7 +26,10 @@
                          ((org-agenda-overriding-header "Delegated")
                           (org-agenda-skip-function #'+agenda-skip-item-if-timestamp))))
             (today '(agenda ""
-                     ((org-agenda-overriding-header "Today")
+
+                     ((org-agenda-start-day "-3d")
+                      (org-agenda-span 'week)
+                      (org-agenda-overriding-header "Today")
                       (org-agenda-clockreport-parameter-plist '(:compact t
                                                                 :link t
                                                                 :maxlevel 3
@@ -30,14 +39,45 @@
             (notes
              '(tags-todo "-bills+outline-project+TODO=\"TODO\""
                ((org-agenda-overriding-header "Unprocessed Notes")
-                (org-agenda-skip-function #'+agenda-skip-items-already-shown)))))
+                (org-agenda-skip-function #'+agenda-skip-items-already-shown))))
+
+            (defaults `(
+                        ;; (org-stuck-projects '("" nil nil ""))
+                        (org-agenda-todo-ignore-scheduled t)
+                        (org-agenda-include-diary nil)
+                        (org-agenda-insert-diary-extract-time nil)
+                        (org-agenda-show-all-dates nil)
+                        (org-agenda-show-inherited-tags nil)
+                        (org-agenda-skip-deadline-if-done t)
+                        (org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
+                        (org-agenda-skip-scheduled-if-done t)
+                        (org-agenda-start-on-weekday nil)
+                        (org-agenda-window-setup 'only-window)
+                        (org-agenda-dim-blocked-tasks 'invisible)
+                        (org-agenda-sorting-strategy '((agenda time-up category-up priority-down todo-state-up)
+                                                       (todo priority-down category-up scheduled-up)
+                                                       (tags priority-down category-up)
+                                                       (search category-up)))
+                        (org-agenda-clock-report-header "\nClocking")
+                        (org-agenda-tags-column -100)
+                        (org-agenda-use-time-grid nil)
+                        (org-agenda-start-with-log-mode '(closed state))
+                        (org-agenda-show-future-repeats nil)
+                        (org-agenda-ignore-properties '(effort appt))
+                        (org-agenda-archives-mode t))))
+
         `(("p" "personal agenda" ,(list todos bills delegated today)
-           ((org-agenda-tag-filter-preset '("-someday" "-ignore" "-work" "-outline"))
-            (org-agenda-archives-mode t)))
+           (,@defaults
+            (org-agenda-tag-filter-preset '("-someday" "-ignore" "-work" "-outline"))))
           ("w" "work agenda" ,(list todos delegated projects today notes)
-           ((org-agenda-clockreport-mode t)
+           (,@defaults
+            (org-agenda-clockreport-mode t)
             (org-agenda-tag-filter-preset (list "-someday" "-ignore" (format "+%s" (timekeep-work-tag))))
-            (org-agenda-archives-mode t))))))
+            (org-agenda-clock-consistency-checks
+             '(:gap-ok-around ("12:20" "12:40" "4:00")
+               :max-duration "10:00"
+               :min-duration 0
+               :max-gap 0)))))))
 
 (after! evil
   ;; FIXME: Figure out why initial state is currently defaulting to `emacs'.
@@ -70,50 +110,11 @@
       :m "-" 'org-agenda-manipulate-query-subtract
       :m "_" 'org-agenda-manipulate-query-subtract-re)
 
-;;; General settings
-
-(setq org-agenda-files (expand-file-name "org-agenda-files" org-directory))
-
-;; (setq org-stuck-projects '("" nil nil ""))
-
-(setq org-agenda-todo-ignore-scheduled t)
-(setq org-agenda-include-diary nil)
-(setq org-agenda-insert-diary-extract-time t)
-(setq org-agenda-search-view-always-boolean t)
-(setq org-agenda-show-all-dates nil)
-(setq org-agenda-show-inherited-tags nil)
-(setq org-agenda-skip-deadline-if-done t)
-(setq org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
-(setq org-agenda-skip-scheduled-if-done t)
-(setq org-agenda-span 'day)
-(setq org-agenda-start-on-weekday nil)
-(setq org-agenda-window-setup 'only-window)
-(setq org-agenda-dim-blocked-tasks 'invisible)
-(setq org-agenda-sorting-strategy '((agenda time-up category-up priority-down todo-state-up)
-                                    (todo priority-down category-up scheduled-up)
-                                    (tags priority-down category-up)
-                                    (search category-up)))
-(setq org-agenda-clock-report-header "\nClocking")
-(setq org-agenda-tags-column -100)
-(setq org-agenda-text-search-extra-files (list (expand-file-name "archive.org" org-directory)))
-(setq org-agenda-use-time-grid nil)
-
-(setq org-agenda-start-with-log-mode '(closed state))
-(setq org-agenda-span 'day)
-(setq org-agenda-show-future-repeats nil)
-(setq org-agenda-ignore-properties '(effort appt))
-(setq org-agenda-clock-consistency-checks
-      '(:gap-ok-around ("12:20" "12:40" "4:00")
-        :max-duration "10:00"
-        :min-duration 0
-        :max-gap 0))
-
 ;; Automatically remove deleted files from agenda
 (define-advice org-check-agenda-file (:override (file) always-remove-missing)
   (unless (file-exists-p file)
     (org-remove-file file)
     (throw 'nextfile t)))
-
 
 ;;; Use page-break-lines to draw separator in org-agenda.
 
