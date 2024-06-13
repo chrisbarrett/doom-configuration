8295c69b77c6846f8d4d4761ccef4900649a32a4
Chris Barrett <chris@walrus.cool>
2024-06-13 20:36:13 +1200

    Bind M-d to dape prefix

--- modules/tools/debugging/config.el
+++ modules/tools/debugging/config.el
@@ -3,4 +3,5 @@
 (after! dape
   (setq dape-buffer-window-arrangement 'gud)
   (setq dape-cwd-fn #'project-root)
+  (map! :map prog-mode-map :niv (kbd "M-d") dape-global-map)
   (add-hook 'dape-on-start-hooks (lambda () (save-some-buffers t t))))
