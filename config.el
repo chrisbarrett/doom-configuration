89a7312e55a75b00c39f9c19e3eb08bfa00c3852
Chris Barrett <chris@walrus.cool>
2024-07-18 09:33:32 +1200

    Add a few more treesit mode remaps

--- config.el
+++ config.el
@@ -124,13 +124,19 @@
 ;; Remap major modes to use treesit modes, including in org-mode.
 
 (defconst +treesit-mode-remaps
-  '((:orig-mode sh-mode :treesit-mode bash-ts-mode)
+  '((:orig-mode c-mode :treesit-mode c-ts-mode :org-src ("C"))
+    (:orig-mode c++-mode :treesit-mode c++-ts-mode :org-src ("cpp" "c++"))
+    (:orig-mode c-or-c++-mode :treesit-mode c-or-c++-ts-mode)
     (:orig-mode conf-toml-mode :treesit-mode toml-ts-mode :org-src ("conf-toml" "toml"))
     (:orig-mode csharp-mode :treesit-mode csharp-ts-mode :org-src ("csharp"))
     (:orig-mode dockerfile-mode :treesit-mode dockerfile-ts-mode :org-src ("dockerfile"))
+    (:orig-mode java-mode :treesit-mode java-ts-mode :org-src ("java"))
     (:orig-mode js-mode :treesit-mode js-ts-mode :org-src "js")
     (:orig-mode json-mode :treesit-mode json-ts-mode :org-src "json")
     (:orig-mode python-mode :treesit-mode python-ts-mode :org-src ("python" "py"))
+    (:orig-mode rust-mode :treesit-mode rust-ts-mode :org-src ("rust"))
+    (:orig-mode sh-mode :treesit-mode bash-ts-mode)
+    (:orig-mode toml-mode :treesit-mode toml-ts-mode :org-src ("toml"))
     (:orig-mode typescript-mode :treesit-mode typescript-ts-mode :org-src ("ts" "typescript"))
     (:orig-mode yaml-mode :treesit-mode yaml-ts-mode :org-src ("yml" "yaml"))))
 
