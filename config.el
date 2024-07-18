8fca4f64b85e2eb5a6d6c0c5818258657cec3c96
Chris Barrett <chris@walrus.cool>
2024-07-18 16:54:53 +1200

    Nest indent-guides setup logic in module test

--- config.el
+++ config.el
@@ -46,10 +46,9 @@
 
 ;;; Indent-guides
 
-(remove-hook! (prog-mode text-mode conf-mode) 'highlight-indent-guides-mode)
-(add-hook! (yaml-mode yaml-ts-mode nxml-mode python-ts-mode) 'highlight-indent-guides-mode)
-
-(after! highlight-indent-guides
+(when (modulep! :ui indent-guides)
+  (remove-hook! (prog-mode text-mode conf-mode) 'highlight-indent-guides-mode)
+  (add-hook! (yaml-mode yaml-ts-mode nxml-mode python-ts-mode) 'highlight-indent-guides-mode)
   (setq highlight-indent-guides-method 'fill)
   (setq highlight-indent-guides-responsive t))
 
