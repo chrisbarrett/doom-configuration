03b6fa87ba9fcdeab3ea15c81709d6d4b35ded3e
Chris Barrett <chris@walrus.cool>
2024-07-25 22:08:10 +1200

    Disable frame decorations entirely on macos

--- modules/ui/doom-exts/+macos.el
+++ modules/ui/doom-exts/+macos.el
@@ -1,5 +1,3 @@
 ;;; ui/doom-exts/+macos.el -*- lexical-binding: t; -*-
 
-(setq ns-use-proxy-icon nil)
-(modify-all-frames-parameters '((ns-appearance . dark)
-                                (ns-transparent-titlebar . t)))
+(modify-all-frames-parameters '((undecorated . t)))
