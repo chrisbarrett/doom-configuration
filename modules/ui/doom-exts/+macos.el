0d841be80981234f7b48d9f76a141e9f79ba4d75
Chris Barrett <chris@walrus.cool>
2024-09-29 09:43:38 +1300

    Add macOS SDK location to Man header file path

--- modules/ui/doom-exts/+macos.el
+++ modules/ui/doom-exts/+macos.el
@@ -1,3 +1,11 @@
 ;;; ui/doom-exts/+macos.el -*- lexical-binding: t; -*-
 
 (modify-all-frames-parameters '((undecorated . t)))
+
+(defun +macos-sdk-header-file-locations ()
+  (let ((sdk-root
+         (string-trim-right (shell-command-to-string "xcrun --show-sdk-path"))))
+    (expand-file-name "usr/include" sdk-root)))
+
+(after! man
+  (pushnew! Man-header-file-path (+macos-sdk-header-file-locations)))
